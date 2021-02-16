f349e983b614bda8a873b15cfa2812d470b547c0
David D. Riddle <ddriddle@illinois.edu>
2021-02-16 14:44:55 -0600

    Change weekold script to print age of oldest file

--- scripts/weekold.py
+++ scripts/weekold.py
@@ -3,14 +3,18 @@ import os
 import sys
 import time
 
-WEEK = 60 * 60 * 24 * 7
+from datetime import datetime, timedelta
 
-def done(old: bool):
+WEEK = timedelta(days=7)
+
+def done(old: bool, oldest=None):
     # https://docs.github.com/en/free-pro-team@latest/actions/reference/workflow-commands-for-github-actions#using-workflow-commands-to-access-toolkit-functions
     github_workflow_cmd = "::set-output name=weekold::"
 
     print(github_workflow_cmd, old, sep='')
     print("Are any of the files one week old or older?", old, file=sys.stderr)
+    if oldest is not None:
+        print("Oldest file is", oldest, "old", file=sys.stderr)
     exit(int(old))
 
 def main():
@@ -20,16 +24,20 @@ def main():
         help='a file to be tested.')
     args = parser.parse_args()
 
-    now = time.time()
+    oldest = timedelta(0)
+    now = datetime.now()
     for filename in args.files:
-        age = now - os.stat(filename).st_mtime
+        age = now - datetime.fromtimestamp(os.stat(filename).st_mtime)
+        if age > oldest:
+            oldest = age
         if age < WEEK:
-            done(False)
+            done(False, oldest)
+
+    return oldest
 
 if __name__ == "__main__":
     try:
-        main()
+        oldest = main()
+        done(True, oldest)
     except Exception as e:
         print(e, file=sys.stderr)
-
-    done(True)
